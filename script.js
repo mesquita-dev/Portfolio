f32ac016f188a82cf5d8a36fa1bcef5c683af0b6
Mesquita-Dev <lmesquita2k@gmail.com>
2022-05-22 09:12:34 -0300

    fixing-js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const tabMenu = document.querySelectorAll (".js-tabmenu li")
 const tabContent = document.querySelectorAll (".js-tabcontent article");
+const tabDec = document.querySelectorAll (".js-dec");
 
 if (tabMenu.length && tabContent.length){
   tabContent[0].classList.add("ativo");
@@ -11,12 +12,19 @@ if (tabMenu.length && tabContent.length){
     tabContent[index].classList.add ("ativo");
   };
 
+  function activeMenu(index){
+    tabDec.forEach((decoracao) => {
+      decoracao.classList.remove ("curso-selecionado");
+    });
+    tabDec[index].classList.add ("curso-selecionado");
+  }
+
   tabMenu.forEach ((itemMenu, index) => {
     itemMenu.addEventListener('click', () => {
       activeTab(index);
     });
     itemMenu.addEventListener ('click', () => {
-      itemMenu.classList.toggle("curso-selecionado");
+      activeMenu(index);
     });
   });
 }
