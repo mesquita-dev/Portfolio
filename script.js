06c1dd1ba6b9be9c04f8f860c4af2d5a87805641
Mesquita-Dev <lmesquita2k@gmail.com>
2022-05-22 11:54:40 -0300

    js-and-animations

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
-const tabMenu = document.querySelectorAll (".js-tabmenu li")
-const tabContent = document.querySelectorAll (".js-tabcontent article");
-const tabDec = document.querySelectorAll (".js-dec");
+function cursos() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent article");
+  const tabDec = document.querySelectorAll(".js-dec");
 
-if (tabMenu.length && tabContent.length){
-  tabContent[0].classList.add("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+    tabDec[0].classList.add("curso-selecionado");
 
-  function activeTab(index){
-    tabContent.forEach((article) => {
-      article.classList.remove("ativo");
+    function activeTab(index) {
+      tabContent.forEach((article) => {
+        article.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    function activeMenu(index) {
+      tabDec.forEach((decoracao) => {
+        decoracao.classList.remove("curso-selecionado");
+      });
+      tabDec[index].classList.add("curso-selecionado");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+      itemMenu.addEventListener("click", () => {
+        activeMenu(index);
+      });
     });
-    tabContent[index].classList.add ("ativo");
-  };
+  }
+}
+cursos();
 
-  function activeMenu(index){
-    tabDec.forEach((decoracao) => {
-      decoracao.classList.remove ("curso-selecionado");
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+   function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
     });
-    tabDec[index].classList.add ("curso-selecionado");
   }
 
-  tabMenu.forEach ((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
-    });
-    itemMenu.addEventListener ('click', () => {
-      activeMenu(index);
-    });
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
   });
 }
-
+initScrollSuave();
