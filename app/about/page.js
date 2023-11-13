1430cf29237549f762cfbd5a77dea7597db4cef3
Lucas Mesquita <lmesquita2k@gmail.com>
2023-11-13 18:04:54 -0300

    feat: some page changing to english

--- app/about/page.js
+++ app/about/page.js
@@ -12,118 +12,142 @@ const About = () => {
       <Header />
 
       <Hero
-        textSpan="Um pouco sobre mim"
-        textTitle="O hobbie que virou um trabalho"
-        textParagraph="Aqui estará algumas informações sobre minha vida tanto pessoalmente quanto profissionalmente."
-        textButton="Saber mais sobre mim"
-        href='https://linkedin.com/in/mesquita-dev'
-        target='blank'
+        textSpan="A little bit about me"
+        textTitle="The hobby that turned into a job"
+        textParagraph="Here is some information about my life both personally and professionally."
+        textButton="Find out more about me"
+        href="https://linkedin.com/in/mesquita-dev"
+        target="blank"
       />
 
       <div>
         <section className="container border-x border-[#F0F0F0] pt-14 sm:pt-28">
           <h2 className="text-black text-4xl font-bold mb-2">
-            Como tudo começou?
+            How it all began?
           </h2>
 
           <p className="max-w-lg text-base mb-2">
-            Tudo começou durante a pandemia do COVID-19, onde resolvi aprender
-            algumas coisas sobre desenvolvimento web e a partir daí comecei a me
-            interessar por essa área.
+            It all started during the COVID-19 pandemic, where I decided to
+            learn a few things about web development and from then on I started
+            interest in this area.
           </p>
 
           <p className="max-w-lg text-base mb-2">
-            Porém, eu notava que as interfaces feitas apenas HTML e CSS não
-            tinham embaçamento nenhum sobre design e resolvi pesquisar mais
-            sobre como poderia melhorar minhas interfaces para deixar mais
-            amigáveis, bonitas e mais usáveis para os usuários, então descobri o
-            UI Design e resolvi aprofundar e estudar para poder implmentar nos
-            meus projetos.
+            However, I noticed that interfaces made only with HTML and CSS do
+            not there was no confusion about design and I decided to research
+            more about how I could improve my interfaces to make more friendly,
+            beautiful and more usable for users, so I discovered the UI Design
+            and decided to deepen and study it to be able to implement it in my
+            projects.
           </p>
 
           <p className="max-w-lg text-base">
-            Obviamente, a primeira interface ficou extremamente feia mas a
-            partir daí vi que existia um caminho a ser seguido. E assim começou
-            a minha jornada como um UI Designer, UX Designer, Desenvolvedor
-            Front-End.
+            Obviously, the first interface was extremely ugly but the From then
+            on I saw that there was a path to be followed. And so it began my
+            journey as a UI Designer, UX Designer, Developer Front End.
           </p>
 
           <h2 className="text-black text-4xl font-bold mb-2 mt-6">
-            O ínicio da jornada profissional
+            The beginning of the professional journey
           </h2>
 
           <p className="max-w-lg text-base mb-2">
-            A minha jornada profissional começou como um estagiário na{" "}
-            <Link href="https://agsix.com.br/" className="underline" target="_blank">Agência SIX</Link>, uma agência digital onde pude ter
-            minha primeira experiência nesse mundo novo para mim. Começou com
-            uma entrega de currículo sem pretensão e acabou virando minha
-            primeira experiência de trabalho.
+            My professional journey began as an intern at{" "}
+            <Link
+              href="https://agsix.com.br/"
+              className="underline"
+              target="_blank"
+            >
+              Agência SIX
+            </Link>
+            , a digital agency where I was able to have my first experience in
+            this new world for me. It started with a resume delivery without
+            pretension and ended up becoming my first work experience.
           </p>
 
           <p className="max-w-lg text-base mb-2">
-            Nessa agência conheci uma das pessoas das quais mais devo na minha
-            vida profissional, o meu amigo{" "}
-            <Link href="https://eddu.dev/" className="underline">Eduardo Fernandes</Link>, me ensinou diversas coisas
-            sobre front-end mas principalmente UI/UX Design onde muito do que
-            sei hoje devo a ele.
+            At this agency I met one of the people I owe the most to in my life.
+            professional life, my friend{" "}
+            <Link href="https://eddu.dev/" className="underline">
+              Eduardo Fernandes
+            </Link>
+            , taught me several things about front-end, but mainly UI/UX Design
+            where I owe much of what I know today to him.
           </p>
 
           <p className="max-w-lg text-base mb-2">
-            Posteriormente, apliquei para uma vaga na{" "}
-            <Link href="https://grantoseguros.com" className="underline" target="_blank">Granto Seguros</Link> para trabalhar como vendedor,
-            porém, no meio do processo seletivo em conversa com a{" "}
-            <Link href="https://www.linkedin.com/in/raquel-santana/" className="underline" target="_blank">Raquel Santana</Link> uma outra pessoa que sou
-            extremamente grato, consegui mudar para Web Designer, uma posição
-            que não havia até o momento na empresa.
+            Later, I applied for a position at{" "}
+            <Link
+              href="https://grantoseguros.com"
+              className="underline"
+              target="_blank"
+            >
+              Granto Seguros
+            </Link>{" "}
+            to work as a salesperson, however, in the middle of the selection
+            process in talk to the{" "}
+            <Link
+              href="https://www.linkedin.com/in/raquel-santana/"
+              className="underline"
+              target="_blank"
+            >
+              Raquel Santana
+            </Link>{" "}
+            Another person, who I am extremely grateful for, managed to move to
+            Web Designer, a position that was not available at the company until
+            now.
           </p>
 
           <p className="max-w-lg text-base mb-2">
-            Mas acabou surgindo uma vaga interna para UI/UX Designer e resolvi
-            aplicar e felizmente consegui conseguir. Hoje estou atuando como
-            UI/UX Designer em um projeto interno para a criação de um dashboard
-            inovador para o modelo de negócio que a Granto Seguros segue.
+            But an internal vacancy for a Product Designer ended up opening up
+            and I decided to apply and luckily I managed to get it. Today I am
+            acting as Product Designer in an internal project to create a
+            dashboard innovative for the business model that Granto Seguros
+            follows.
           </p>
         </section>
 
         <section className="container border-x border-[#F0F0F0] pt-14  pb-14 sm:pb-28">
-          <h2 className="text-black text-4xl font-bold mb-4">
-            Carreira até o momento
-          </h2>
+          <h2 className="text-black text-4xl font-bold mb-4">Career to date</h2>
 
           <div>
             <div className="flex justify-between max-w-2xl border-b border-[#F0F0F0] mb-4 pb-4">
               <div>
-                <h3 className="font-medium text-lg sm:text-xl">UI/UX Designer na Granto Seguros</h3>
+                <h3 className="font-medium text-lg sm:text-xl">
+                  Product Designer at Granto Seguros
+                </h3>
                 <p className="text-sm">
                   UI Design • UX Design • Design System • React • Typescript
                 </p>
               </div>
-              <h4 className="text-sm sm:text-base">Jun 2023 — Atualmente</h4>
+              <h4 className="text-sm sm:text-base">Jun 2023 — Actually</h4>
             </div>
 
             <div className="flex justify-between max-w-2xl border-b border-[#F0F0F0] mb-4 pb-4">
               <div>
-                <h3 className="font-medium text-lg sm:text-xl">Web Designer na Granto Seguros</h3>
+                <h3 className="font-medium text-lg sm:text-xl">
+                  Web Designer at Granto Seguros
+                </h3>
                 <p className="text-sm">
                   UI Design • UX Design • Wordpress • Elementor • CRO
                 </p>
               </div>
-              <h4 className="text-sm sm:text-base">Dez 2022 — Jun 2023</h4>
+              <h4 className="text-sm sm:text-base">Dec 2022 — Jun 2023</h4>
             </div>
 
             <div className="flex justify-between max-w-2xl pb-4">
               <div>
-                <h3 className="font-medium text-lg sm:text-xl">Front-end / UI & UX Designer na Agência SIX</h3>
-                <p className="text-sm">
-                  UI/UX Design • HTML • CSS • Figma
-                </p>
+                <h3 className="font-medium text-lg sm:text-xl">
+                  Front-end / UI & UX Designer at Agência SIX
+                </h3>
+                <p className="text-sm">UI/UX Design • HTML • CSS • Figma</p>
               </div>
-              <h4 className="text-sm sm:text-base">Jun 2022 — Set 2022</h4>
+              <h4 className="text-sm sm:text-base">Jun 2022 — Sep 2022</h4>
             </div>
           </div>
         </section>
       </div>
-    
+
       <Footer />
     </>
   );
--- components/header/header.js
+++ components/header/header.js
@@ -1,23 +1,36 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='bg-black'>
-      <header className='container border border-[#202020] border-t-0 flex justify-between items-center py-8'>
-        <Link href="/" className='text-white text-xl font-light tracking-wide'>lucas.design</Link>
+    <div className="bg-black">
+      <header className="container border border-[#202020] border-t-0 flex justify-between items-center py-8">
+        <Link href="/" className="text-white text-xl font-light tracking-wide">
+          lucas.design
+        </Link>
 
-        <ul className='flex gap-6'>
+        <ul className="flex gap-6">
           <li>
-            <Link href='/about' className='text-[#808080] text-base tracking-tight font-light hover:text-white'>sobre</Link>
+            <Link
+              href="/about"
+              className="text-[#808080] text-base tracking-tight font-light hover:text-white"
+            >
+              about
+            </Link>
           </li>
           <li>
-            <Link href='https://read.cv/lucasmesquita' className='text-[#808080] text-base tracking-tight font-light hover:text-white' target='_blank'>currículo</Link>
-          </li> 
+            <Link
+              href="https://read.cv/lucasmesquita"
+              className="text-[#808080] text-base tracking-tight font-light hover:text-white"
+              target="_blank"
+            >
+              resume
+            </Link>
+          </li>
         </ul>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
