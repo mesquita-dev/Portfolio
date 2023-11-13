790ade644b643fc82f639992c028d4a2f37e487d
Lucas Mesquita <lmesquita2k@gmail.com>
2023-11-13 18:09:41 -0300

    feat: changin form

--- components/form/form.js
+++ components/form/form.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useForm } from "react-hook-form";
 import emailjs from "@emailjs/browser";
@@ -27,78 +27,78 @@ const Form = () => {
   };
 
   return (
-      <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <div className="relative">
-          <input
-            type="text"
-            id="name"
-            {...register("name", {
-              required: {
-                value: true,
-                message: "Preencha o seu nome",
-              },
-            })}
-            placeholder="Digite seu nome"
-            autoComplete="off"
-            className="peer h-8 w-full bg-transparent border-b border-[#202020] text-white placeholder-transparent focus:outline-none focus:border-white mb-8 max-w-lg"
-          />
-          <label
-            htmlFor="name"
-            className="absolute left-0 -top-4 text-[#BABABA] cursor-pointer text-sm peer-placeholder-shown:text-base transition-all peer-placeholder-shown:text-[#BABABA] peer-placeholder-shown:top-1 peer-focus:-top-4 peer-focus:text-[#BABABA] peer-focus:text-sm"
-          >
-            Digite seu nome
-          </label>
-          <p className="text-red-500 -mt-7 mb-7">{errors.name?.message}</p>
-        </div>
-
-        <div className="relative">
-          <input
-            id="email"
-            type="email"
-            {...register("email", {
-              pattern: {
-                value:
-                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                message: "Email inválido.",
-              },
-            })}
-            placeholder="Digite seu email"
-            autoComplete="off"
-            className="peer h-8 w-full bg-transparent border-b border-[#202020] text-white placeholder-transparent focus:outline-none focus:border-white mb-8 max-w-lg"
-          />
-          <label
-            htmlFor="email"
-            className="absolute left-0 -top-4 text-[#BABABA] cursor-pointer text-sm peer-placeholder-shown:text-base transition-all peer-placeholder-shown:text-[#BABABA] peer-placeholder-shown:top-1 peer-focus:-top-4 peer-focus:text-[#BABABA] peer-focus:text-sm"
-          >
-            Digite seu email
-          </label>
-          <p className="text-red-500 -mt-7 mb-7">{errors.email?.message}</p>
-        </div>
-
-        <div className="relative">
-          <textarea
-            id="message"
-            {...register("message")}
-            placeholder="Digite sua mensagem..."
-            className="peer h-8 w-full bg-transparent border-b border-[#202020] text-white placeholder-transparent focus:outline-none focus:border-white max-w-lg resize-none"
-          />
-          <label
-            htmlFor="message"
-            className="absolute left-0 -top-4 text-[#BABABA] cursor-pointer text-sm peer-placeholder-shown:text-base transition-all peer-placeholder-shown:text-[#BABABA] peer-placeholder-shown:top-1 peer-focus:-top-4 peer-focus:text-[#BABABA] peer-focus:text-sm break-words"
-          >
-            Digite sua mensagem
-          </label>
-        </div>
-
-        <Button
-          type="submit"
-          variant="secondary"
-          className="mt-8 flex items-center gap-4"
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
+      <div className="relative">
+        <input
+          type="text"
+          id="name"
+          {...register("name", {
+            required: {
+              value: true,
+              message: "Fill in your name",
+            },
+          })}
+          placeholder="Type your name"
+          autoComplete="off"
+          className="peer h-8 w-full bg-transparent border-b border-[#202020] text-white placeholder-transparent focus:outline-none focus:border-white mb-8 max-w-lg"
+        />
+        <label
+          htmlFor="name"
+          className="absolute left-0 -top-4 text-[#BABABA] cursor-pointer text-sm peer-placeholder-shown:text-base transition-all peer-placeholder-shown:text-[#BABABA] peer-placeholder-shown:top-1 peer-focus:-top-4 peer-focus:text-[#BABABA] peer-focus:text-sm"
         >
-          <PaperPlaneTilt />
-          Enviar sua mensagem
-        </Button>
-      </form>
+          Type your name
+        </label>
+        <p className="text-red-500 -mt-7 mb-7">{errors.name?.message}</p>
+      </div>
+
+      <div className="relative">
+        <input
+          id="email"
+          type="email"
+          {...register("email", {
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email.",
+            },
+          })}
+          placeholder="Type your email"
+          autoComplete="off"
+          className="peer h-8 w-full bg-transparent border-b border-[#202020] text-white placeholder-transparent focus:outline-none focus:border-white mb-8 max-w-lg"
+        />
+        <label
+          htmlFor="email"
+          className="absolute left-0 -top-4 text-[#BABABA] cursor-pointer text-sm peer-placeholder-shown:text-base transition-all peer-placeholder-shown:text-[#BABABA] peer-placeholder-shown:top-1 peer-focus:-top-4 peer-focus:text-[#BABABA] peer-focus:text-sm"
+        >
+          Type your email
+        </label>
+        <p className="text-red-500 -mt-7 mb-7">{errors.email?.message}</p>
+      </div>
+
+      <div className="relative">
+        <textarea
+          id="message"
+          {...register("message")}
+          placeholder="Say your message..."
+          className="peer h-8 w-full bg-transparent border-b border-[#202020] text-white placeholder-transparent focus:outline-none focus:border-white max-w-lg resize-none"
+        />
+        <label
+          htmlFor="message"
+          className="absolute left-0 -top-4 text-[#BABABA] cursor-pointer text-sm peer-placeholder-shown:text-base transition-all peer-placeholder-shown:text-[#BABABA] peer-placeholder-shown:top-1 peer-focus:-top-4 peer-focus:text-[#BABABA] peer-focus:text-sm break-words"
+        >
+          Say your message...
+        </label>
+      </div>
+
+      <Button
+        type="submit"
+        variant="secondary"
+        className="mt-8 flex items-center gap-4"
+      >
+        <PaperPlaneTilt />
+        Send your message
+      </Button>
+    </form>
   );
 };
 
